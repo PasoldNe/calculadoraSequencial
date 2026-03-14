5019f893acb920858000089f73e333db6fbfcfab
Pasold <vitorpasold4@gmail.com>
2026-03-14 11:30:55 -0700

    leitura de valores

--- calculadoraSequencial/src/App.java
+++ calculadoraSequencial/src/App.java
@@ -5,12 +5,17 @@ public class App {
         Scanner scan = new Scanner();
 
         float num[];
-        char operador;
+        char operador[]; 
         float resultado = 0;
 
         System.out.println("Agora calcularemos varios numeros um atras do outro :0");
         
-        
+        for ( int i = 0 ; operador != 'f'; i++){
+            System.out.println("\n\nDigite o " + i + "º numero desejado");
+            num[i] = scan.nextFloat();
+            System.ou.println("Digite o operador (+, -, *, /)");
+            operador[i] = scan.next();
+        }
         
     }
 }
