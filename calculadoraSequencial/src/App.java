6cdae7d5fa6663822375d5fba169ecf9fc28c987
Pasold <vitorpasold4@gmail.com>
2026-03-14 11:46:33 -0700

    malditas linhas vermelhas resolvidas

--- calculadoraSequencial/src/App.java
+++ calculadoraSequencial/src/App.java
@@ -1,19 +1,20 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Scanner scan = new Scanner();
+        Scanner scan = new Scanner(System.in);
 
-        float num[];
-        char operador[]; 
+        float[] num = {};
+        String[] operador = {}; 
         float resultado = 0;
 
         System.out.println("Agora calcularemos varios numeros um atras do outro :0");
         
-        for ( int i = 0 ; operador != 'f'; i++){
+        for ( int i = 0 ; operador[i] != "f"; i++){
             System.out.println("\n\nDigite o " + i + "º numero desejado");
             num[i] = scan.nextFloat();
-            System.ou.println("Digite o operador (+, -, *, /)");
+            System.out.println("Digite o operador (+, -, *, /)");
             operador[i] = scan.next();
         }
         
