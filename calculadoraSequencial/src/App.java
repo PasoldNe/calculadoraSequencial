b1b9d91e24bf526432d63b16d0fabcc3be27c50e
Pasold <vitorpasold4@gmail.com>
2026-03-14 13:30:49 -0700

    adicionado terminar e contador

--- calculadoraSequencial/src/App.java
+++ calculadoraSequencial/src/App.java
@@ -8,14 +8,21 @@ public class App {
         float[] num = new float[15];
         String[] operador = new String[14]; 
         float resultado = 0;
+        boolean continuar = true;
 
         System.out.println("Agora calcularemos varios numeros um atras do outro :0");
         
-        for ( int i = 0 ; operador[i] != "f"; i++){
-            System.out.println("\n\nDigite o " + i + "º numero desejado");
+        for ( int i = 0 ; continuar; i++){
+            int contador = i;
+            contador += 1;
+            System.out.println("\n\nDigite o " + contador + "º numero desejado");
             num[i] = scan.nextFloat();
             System.out.println("Digite o operador (+, -, *, /)");
             operador[i] = scan.next();
+
+            if(operador[i].equals("f") || operador[i].equals("F")){
+                continuar = false;
+            }
         }
         
     }
