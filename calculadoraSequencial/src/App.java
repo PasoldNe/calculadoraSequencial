d985b15b7acbf759075ea937ff9aee8ee5c124a0
Pasold <vitorpasold4@gmail.com>
2026-03-14 11:05:21 -0700

    esboco antes da cagada

--- calculadoraSequencial/src/App.java
+++ calculadoraSequencial/src/App.java
@@ -1,4 +1,16 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
+        Scanner scan = new Scanner();
+
+        float num[];
+        char operador;
+        float resultado = 0;
+
+        System.out.println("Agora calcularemos varios numeros um atras do outro :0");
+        
+        
+        
     }
 }
